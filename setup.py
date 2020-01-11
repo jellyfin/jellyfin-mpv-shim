c42ce6cfa3b3d553ad80121b5fd7ce3ebe33d6f6
Ian Walton <ian@iwalton.com>
2020-01-10 22:53:31 -0500

    Initial renames for project.

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='plex-mpv-shim',
-    version='1.5.2',
+    name='jellyfin-mpv-shim',
+    version='0.0.1',
     author="Ian Walton",
     author_email="iwalton3@gmail.com",
-    description="Cast media from Plex Mobile and Web apps to MPV. (Unofficial)",
+    description="Cast media from Jellyfin Mobile and Web apps to MPV. (Unofficial)",
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/iwalton3/plex-mpv-shim",
+    url="https://github.com/iwalton3/jellyfin-mpv-shim",
     packages=['plex_mpv_shim'],
     entry_points={
         'console_scripts': [
-            'plex-mpv-shim=plex_mpv_shim.mpv_shim:main',
+            'plex-mpv-shim=jellyfin_mpv_shim.mpv_shim:main',
         ]
     },
     classifiers=[
