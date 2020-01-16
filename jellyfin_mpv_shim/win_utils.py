9127eaa9691f0191db1fe6a4720c8b853d45f8f2
Ian Walton <ian@iwalton.com>
2020-01-16 01:26:27 -0500

    Windows-specific fix for raise_mpv flashing orange.

--- jellyfin_mpv_shim/win_utils.py
+++ jellyfin_mpv_shim/win_utils.py
@@ -1,14 +1,25 @@
 import win32gui
+import traceback
 
 def windowEnumerationHandler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
 def raise_mpv():
-    results = []
-    top_windows = []
-    win32gui.EnumWindows(windowEnumerationHandler, top_windows)
-    for i in top_windows:
-        if "mpv" in i[1].lower():
-            win32gui.ShowWindow(i[0],5)
-            win32gui.SetForegroundWindow(i[0])
-            break
+    # This workaround is madness. Apparently SetForegroundWindow
+    # won't work randomly, so I have to call ShowWindow twice.
+    # Once to hide the window, and again to successfully raise the window.
+    try:
+        top_windows = []
+        fg_win = win32gui.GetForegroundWindow()
+        win32gui.EnumWindows(windowEnumerationHandler, top_windows)
+        for i in top_windows:
+            if " - mpv" in i[1].lower():
+                if i[0] != fg_win:
+                    win32gui.ShowWindow(i[0], 6) # Minimize
+                    win32gui.ShowWindow(i[0], 9) # Un-minimize
+                break
+
+    except Exception:
+        print("Could not raise MPV.")
+        traceback.print_exc()
+
