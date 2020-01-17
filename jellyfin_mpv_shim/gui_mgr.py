7ce6b31d4ecbb1f34ed750896f9c40a199429372
Ian Walton <ian@iwalton.com>
2020-01-16 21:48:40 -0500

    Detect if bundled icon should be used.

--- jellyfin_mpv_shim/gui_mgr.py
+++ jellyfin_mpv_shim/gui_mgr.py
@@ -15,8 +15,6 @@ from .constants import USER_APP_NAME, APP_NAME
 from .conffile import confdir
 from .clients import clientManager
 
-import os
-import sys
 if (sys.platform.startswith("win32") or sys.platform.startswith("cygwin")) and getattr(sys, 'frozen', False):
     # Detect if bundled via pyinstaller.
     # From: https://stackoverflow.com/questions/404744/
