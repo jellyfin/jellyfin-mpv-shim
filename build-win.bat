365c1dadb5b9232adf2f6620266521507cc309cd
Ian Walton <ian@iwalton.com>
2020-02-22 00:50:34 -0500

    Add Windows build scripts.

---
+++ build-win.bat
@@ -0,0 +1,4 @@
+@echo off
+rd /s /q __pycache__ dist build
+pyinstaller -wF --add-binary "mpv-1.dll;." --add-data "jellyfin_mpv_shim\systray.png;jellyfin_mpv_shim" --add-data "jellyfin_mpv_shim\display_mirror\index.html;jellyfin_mpv_shim\display_mirror" --add-data "jellyfin-chromecast\css\jellyfin.css;jellyfin_mpv_shim\display_mirror" --add-binary "Microsoft.Toolkit.Forms.UI.Controls.WebView.dll;." --icon media.ico run.py
+
