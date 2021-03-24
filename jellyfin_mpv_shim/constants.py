db29196b64cee2e4faddcc8bed66e59fc0a36d91
Ian Walton <ian@iwalton.com>
2021-03-24 19:37:55 -0400

    Up release to 1.10.1.

--- jellyfin_mpv_shim/constants.py
+++ jellyfin_mpv_shim/constants.py
@@ -1,6 +1,6 @@
 APP_NAME = "jellyfin-mpv-shim"
 USER_APP_NAME = "Jellyfin MPV Shim"
-CLIENT_VERSION = "1.10.0"
+CLIENT_VERSION = "1.10.1"
 USER_AGENT = "Jellyfin-MPV-Shim/%s" % CLIENT_VERSION
 CAPABILITIES = {
     "PlayableMediaTypes": "Video",
