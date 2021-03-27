114a9000bef4b8192b3564811db6ee22466c1f6c
Ian Walton <ian@iwalton.com>
2021-03-27 09:36:31 -0400

    #179 Add debug builds to CI.

--- build-win-dbg.bat
+++ build-win-dbg.bat
@@ -1,5 +1,6 @@
 @echo off
-rd /s /q __pycache__ dist build
+rd /s /q __pycache__ build 
+rd /s /q dist\run-desktop
 set PATH=%PATH%;%CD%
 rem Edge-based build
 rem pyinstaller -c --add-binary "mpv-1.dll;." --add-data "jellyfin_mpv_shim\systray.png;jellyfin_mpv_shim" --add-data "jellyfin_mpv_shim\display_mirror\index.html;jellyfin_mpv_shim\display_mirror" --add-data "jellyfin_mpv_shim\webclient_view\webclient;jellyfin_mpv_shim\webclient_view\webclient" --add-data "jellyfin_mpv_shim\display_mirror\jellyfin.css;jellyfin_mpv_shim\display_mirror" --add-binary "Microsoft.Toolkit.Forms.UI.Controls.WebView.dll;." --icon jellyfin.ico run-desktop-edge.py
@@ -8,6 +9,5 @@ pyinstaller -c --add-binary "mpv-1.dll;." --add-data "jellyfin_mpv_shim\mouse.lu
 xcopy /E /Y cef\cefpython3 dist\run-desktop\
 del dist\run-desktop\run-desktop.exe.manifest
 copy hidpi.manifest dist\run-desktop\run-desktop.exe.manifest
-rd /s /q __pycache__ build
-pyinstaller -cF --add-binary "mpv-1.dll;." --add-data "jellyfin_mpv_shim\mouse.lua;jellyfin_mpv_shim" --add-data "jellyfin_mpv_shim\systray.png;jellyfin_mpv_shim" --add-data "jellyfin_mpv_shim\default_shader_pack;jellyfin_mpv_shim\default_shader_pack" --add-data "jellyfin_mpv_shim\messages;jellyfin_mpv_shim\messages" --add-data "jellyfin_mpv_shim\display_mirror\index.html;jellyfin_mpv_shim\display_mirror" --add-data "jellyfin_mpv_shim\display_mirror\jellyfin.css;jellyfin_mpv_shim\display_mirror" --add-binary "Microsoft.Toolkit.Forms.UI.Controls.WebView.dll;." --exclude-module cefpython3 --icon jellyfin.ico run.py
-pause
+rem rd /s /q __pycache__ build
+rem pyinstaller -cF --add-binary "mpv-1.dll;." --add-data "jellyfin_mpv_shim\mouse.lua;jellyfin_mpv_shim" --add-data "jellyfin_mpv_shim\systray.png;jellyfin_mpv_shim" --add-data "jellyfin_mpv_shim\default_shader_pack;jellyfin_mpv_shim\default_shader_pack" --add-data "jellyfin_mpv_shim\messages;jellyfin_mpv_shim\messages" --add-data "jellyfin_mpv_shim\display_mirror\index.html;jellyfin_mpv_shim\display_mirror" --add-data "jellyfin_mpv_shim\display_mirror\jellyfin.css;jellyfin_mpv_shim\display_mirror" --add-binary "Microsoft.Toolkit.Forms.UI.Controls.WebView.dll;." --exclude-module cefpython3 --icon jellyfin.ico run.py
