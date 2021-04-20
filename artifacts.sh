40b2ec2a7934717a965b4de8edb3fdf374806003
Ian Walton <ian@iwalton.com>
2021-04-19 20:24:34 -0400

    Fix artifacts.

--- artifacts.sh
+++ artifacts.sh
@@ -4,7 +4,7 @@ version=$(cat jellyfin_mpv_shim/constants.py | grep '^CLIENT_VERSION' | cut -d '
 if [[ "$1" == "standard" ]]
 then
     cp dist/jellyfin-mpv-shim_version_installer.exe publish/Installer/jellyfin-mpv-shim_${version}_installer.exe || exit 1
-    cp dist/run.exe publish/Shim/jellyfin-mpv-shim_${version}.exe || exit 1
+    cp dist/run.exe publish/Portable/jellyfin-mpv-shim_${version}_portable.exe || exit 1
     mv dist/run publish/Debug/ || exit 1
 elif [[ "$1" == "legacy" ]]
 then
