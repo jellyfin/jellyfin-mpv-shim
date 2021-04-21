dc948f298146306c5a817e52257ace87fece2b3d
Ian Walton <ian@iwalton.com>
2021-04-21 17:22:10 -0400

    Add HiDPI manifest and remove portable mode. (#211)

--- artifacts.sh
+++ artifacts.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-mkdir -p publish publish/Portable publish/Installer publish/InstallerLegacy publish/Debug
+mkdir -p publish publish/Installer publish/InstallerLegacy publish/Debug
 version=$(cat jellyfin_mpv_shim/constants.py | grep '^CLIENT_VERSION' | cut -d '"' -f 2)
 if [[ "$1" == "standard" ]]
 then
     cp dist/jellyfin-mpv-shim_version_installer.exe publish/Installer/jellyfin-mpv-shim_${version}_installer.exe || exit 1
-    cp dist/run.exe publish/Portable/jellyfin-mpv-shim_${version}_portable.exe || exit 1
     #mv dist/run publish/Debug/ || exit 1
 elif [[ "$1" == "legacy" ]]
 then
--- build-win-32.bat
+++ build-win-32.bat
@@ -3,5 +3,7 @@ rd /s /q __pycache__ dist build
 set PATH=%PATH%;%CD%
 pyinstaller -w --add-binary "mpv-1.dll;." --add-data "jellyfin_mpv_shim\mouse.lua;jellyfin_mpv_shim" --hidden-import pystray._win32 --add-data "jellyfin_mpv_shim\default_shader_pack;jellyfin_mpv_shim\default_shader_pack" --add-data "jellyfin_mpv_shim\messages;jellyfin_mpv_shim\messages" --add-data "jellyfin_mpv_shim\systray.png;jellyfin_mpv_shim" --add-data "jellyfin_mpv_shim\display_mirror\index.html;jellyfin_mpv_shim\display_mirror" --add-data "jellyfin_mpv_shim\display_mirror\jellyfin.css;jellyfin_mpv_shim\display_mirror" --add-binary "Microsoft.Toolkit.Forms.UI.Controls.WebView.dll;." --icon jellyfin.ico run.py
 if %errorlevel% neq 0 exit /b %errorlevel%
+del dist\run\run.exe.manifest
+copy hidpi.manifest dist\run\run.exe.manifest
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "Jellyfin MPV Shim.iss"
 if %errorlevel% neq 0 exit /b %errorlevel%
