4d10c9a46a497d0961db6974acae88de6b685079
Ian Walton <ian@iwalton.com>
2022-06-19 13:38:03 -0400

    Update translation files.

--- regen_pot.sh
+++ regen_pot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pygettext --default-domain=base -o jellyfin_mpv_shim/messages/base.pot jellyfin_mpv_shim/*.py jellyfin_mpv_shim/**/*.py
+pygettext3 --default-domain=base -o jellyfin_mpv_shim/messages/base.pot jellyfin_mpv_shim/*.py jellyfin_mpv_shim/**/*.py
 find -iname '*.po' | while read -r file
 do
     msgmerge --update "$file" --backup=none --previous jellyfin_mpv_shim/messages/base.pot
