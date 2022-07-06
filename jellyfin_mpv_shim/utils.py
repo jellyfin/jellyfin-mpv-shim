4b0ab13a79fe2b2b4c921fcc632484588cea69e6
Ian Walton <ian@iwalton.com>
2022-07-05 23:14:59 -0400

    Add embedded dvbsub support to profile.

--- jellyfin_mpv_shim/utils.py
+++ jellyfin_mpv_shim/utils.py
@@ -163,6 +163,7 @@ def get_profile(
             #    "Method": "External"
             # },
             {"Format": "dvdsub", "Method": "Embed"},
+            {"Format": "dvbsub", "Method": "Embed"},
             # {
             #    "Format": "dvdsub",
             #    "Method": "External"
