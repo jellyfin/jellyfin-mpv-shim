203f96b172883f5781c3596c9fa5384e814c3909
Ian Walton <ian@iwalton.com>
2023-02-14 18:54:31 -0500

    Clean up and add fallback title for discord presence. (#310)

--- jellyfin_mpv_shim/rich_presence.py
+++ jellyfin_mpv_shim/rich_presence.py
@@ -24,19 +24,24 @@ def send_presence(
     if playback_time is not None and duration is not None and playing:
         start = int(time.time() - playback_time)
         end = int(start + duration)
-    RPC.set_activity(
-        state=subtitle,
-        details=title,
-        instance=False,
-        large_image="jellyfin2",
-        start=start,
-        end=end,
-        large_text="Jellyfin",
-        small_image=small_image,
-        party_id=str(hash(syncplay_group)),
-        party_size=[1, 100],
-        join=syncplay_group,
-    )
+
+    payload = {
+        "state": subtitle if subtitle else "Unknown Media",
+        "details": title,
+        "instance": False,
+        "large_image": "jellyfin2",
+        "start": start,
+        "end": end,
+        "large_text": "Jellyfin",
+        "small_image": small_image,
+    }
+
+    if syncplay_group:
+        payload["party_id"] = str(hash(syncplay_group))
+        payload["party_size"] = [1, 100]
+        payload["join"] = syncplay_group
+
+    RPC.set_activity(**payload)
 
 
 def clear_presence():
